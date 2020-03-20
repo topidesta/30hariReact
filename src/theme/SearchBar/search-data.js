9b74a349396dff7719ad1ae2c58653bc8b7ce128
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2020-03-20 15:08:45 +0700

    searchbuild

--- src/theme/SearchBar/search-data.js
+++ src/theme/SearchBar/search-data.js
@@ -1,25 +1,11 @@
 
     export default [
   {
-    "title": "Hello",
-    "type": 0,
-    "sectionRef": "#",
-    "url": "blog/hello-world",
-    "content": "Welcome to this blog. This blog is created with Docusaurus 2 alpha.This is a test post.A whole bunch of other information."
-  },
-  {
-    "title": "Hola",
-    "type": 0,
-    "sectionRef": "#",
-    "url": "blog/hola",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet"
-  },
-  {
     "title": "Welcome",
     "type": 0,
     "sectionRef": "#",
-    "url": "blog/welcome",
-    "content": "Blog features are powered by the blog plugin. Simply add files to the blog directory. It supports tags as well!Delete the whole directory if you don't want the blog features. As simple as that!"
+    "url": "blog/Welcome",
+    "content": "Selamat Menikmati, maaf jika tidak sempurna dalam translate bahasa inggrisnya."
   },
   {
     "title": "Style Guide#H1 - Create the best documentation",
@@ -139,6 +125,97 @@
     "sectionRef": "#",
     "url": "docs/doc3",
     "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac euismod odio, eu consequat dui. Nullam molestie consectetur risus id imperdiet. Proin sodales ornare turpis, non mollis massa ultricies id. Nam at nibh scelerisque, feugiat ante non, dapibus tortor. Vivamus volutpat diam quis tellus elementum bibendum. Praesent semper gravida velit quis aliquam. Etiam in cursus neque. Nam lectus ligula, malesuada et mauris a, bibendum faucibus mi. Phasellus ut interdum felis. Phasellus in odio pulvinar, porttitor urna eget, fringilla lectus. Aliquam sollicitudin est eros. Mauris consectetur quam vitae mauris interdum hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis et egestas libero, imperdiet faucibus ipsum. Sed posuere eget urna vel feugiat. Vivamus a arcu sagittis, fermentum urna dapibus, congue lectus. Fusce vulputate porttitor nisl, ac cursus elit volutpat vitae. Nullam vitae ipsum egestas, convallis quam non, porta nibh. Morbi gravida erat nec neque bibendum, eu pellentesque velit posuere. Fusce aliquam erat eu massa eleifend tristique.Sed consequat sollicitudin ipsum eget tempus. Integer a aliquet velit. In justo nibh, pellentesque non suscipit eget, gravida vel lacus. Donec odio ante, malesuada in massa quis, pharetra tristique ligula. Donec eros est, tristique eget finibus quis, semper non nisl. Vivamus et elit nec enim ornare placerat. Sed posuere odio a elit cursus sagittis.Phasellus feugiat purus eu tortor ultrices finibus. Ut libero nibh, lobortis et libero nec, dapibus posuere eros. Sed sagittis euismod justo at consectetur. Nulla finibus libero placerat, cursus sapien at, eleifend ligula. Vivamus elit nisl, hendrerit ac nibh eu, ultrices tempus dui. Nam tellus neque, commodo non rhoncus eu, gravida in risus. Nullam id iaculis tortor.Nullam at odio in sem varius tempor sit amet vel lorem. Etiam eu hendrerit nisl. Fusce nibh mauris, vulputate sit amet ex vitae, congue rhoncus nisl. Sed eget tellus purus. Nullam tempus commodo erat ut tristique. Cras accumsan massa sit amet justo consequat eleifend. Integer scelerisque vitae tellus id consectetur."
+  },
+  {
+    "title": "Hari `#1`",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari1",
+    "content": ""
+  },
+  {
+    "title": "Hari Ke-10",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari10",
+    "content": ""
+  },
+  {
+    "title": "Hari Pertama",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari1test",
+    "content": ""
+  },
+  {
+    "title": "Hari Ke-02",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari2",
+    "content": ""
+  },
+  {
+    "title": "Hari Ke-03",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari3",
+    "content": ""
+  },
+  {
+    "title": "Hari Ke-04",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari4",
+    "content": ""
+  },
+  {
+    "title": "Hari `#5`",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari5",
+    "content": ""
+  },
+  {
+    "title": "Hari Ke-06",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari6",
+    "content": ""
+  },
+  {
+    "title": "Hari Ke-07",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari7",
+    "content": ""
+  },
+  {
+    "title": "Hari Ke-08",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari8",
+    "content": ""
+  },
+  {
+    "title": "Hari Ke-09",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/hari9",
+    "content": ""
+  },
+  {
+    "title": "Pendahuluan",
+    "type": 0,
+    "sectionRef": "#",
+    "url": "docs/intro",
+    "content": ""
+  },
+  {
+    "title": "Bismillahirrohmanirrohim",
+    "type": 1,
+    "pageTitle": "Pendahuluan",
+    "url": "docs/intro#bismillahirrohmanirrohim",
+    "content": ""
   },
   {
     "title": "Powered by MDX",
