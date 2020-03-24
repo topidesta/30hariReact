9e6511f0fcf589ea077ee9fc7bae6bd9674f5940
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2020-03-24 15:48:31 +0700

    Update

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,6 @@
 module.exports = {
   tigapuluhhari: {
-    Intro: ["intro"],
-    Buku: ["hari1", "hari2", "hari3", "hari4"],
-    Play: ["hari1code", "hari3code"]
+    Intro: ["intro", "play"],
+    Buku: ["hari1", "hari2", "hari3", "hari4"]
   }
 };
